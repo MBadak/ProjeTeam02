f985789d03d7f2af5e8bed19bcaad4a997ccf0ed
Mustafa <m.badak99@gmail.com>
2023-09-02 22:04:24 +0200

    02.09.23mv1

---
+++ src/test/java/techproed/pages/MainLoginPage.java
@@ -0,0 +1,26 @@
+package techproed.pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import techproed.utilities.Driver;
+
+public class MainLoginPage {
+    public MainLoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+
+    }
+
+    @FindBy(xpath = "//*[@class='header_link ms-2']")
+    public WebElement mainLogin;
+
+    @FindBy(xpath = "(//*[@class='form-control'])[1]")
+    public WebElement username;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[@class='fw-semibold btn btn-primary']")
+    public WebElement login;
+
+
+}
