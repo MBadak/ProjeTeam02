d8533849e41d4b04231906a6a256acb38111423e
Mustafa <m.badak99@gmail.com>
2023-09-12 23:12:36 +0200

    12.06.23Mv1

--- src/test/java/techproed/stepDefinition/US06_ApiStepDefs.java
+++ src/test/java/techproed/stepDefinition/US06_ApiStepDefs.java
@@ -1,27 +1,48 @@
 package techproed.stepDefinition;
 
 import io.cucumber.java.en.*;
+import io.restassured.response.Response;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import techproed.pojos.ResponsePojo;
+import techproed.pojos.ViceDeanPojo;
 import techproed.utilities.Driver;
 
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 import static techproed.base_url.ManagementonSchoolsUrl.spec;
 
 public class US06_ApiStepDefs {
-    // https://managementonschools.com/app/vicedean/getViceDeanById/64
+    ViceDeanPojo expectedData;
+    Response response;
+    ResponsePojo actualData;
+
+    //https://managementonschools.com/app/vicedean/save
     @Given("GET Request gonderilir.")
     public void get_request_gonderilir() {
         //Set the Url
-        spec.pathParams("first","vicedean","second", "getViceDeanById", "thirt",64);
+        spec.pathParams("first","vicedean","second", "save");
+        //Set the expected data
+       expectedData = new ViceDeanPojo("1991-11-11","Deutschland","MALE","Sevde","123456789Aa1.","123-456-7890","123-32-7894","Sevde","Sevde123");
+
+    }
+    @Given("GET Request {string} gonderilir.")
+    public void get_request_gonderilir(String str) {
+        //Set the Url
+        spec.pathParams("first","vicedean","second", "save");
         //Set the expected
+        expectedData = new ViceDeanPojo("1991-11-11","Deutschland","MALE","Sevde","123456789Aa1.","123-456-7890","123-32-7894","Sevde","Sevde123");
+
+        // Send the request and get the response
+        response = given(spec).body(expectedData).when().post("{first}/{second}");
+        actualData = response.as(ResponsePojo.class);
 
     }
     @Then("Status kodu dogrula.")
     public void status_kodu_dogrula() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertEquals(200, response.statusCode());
     }
     @Then("Response'den gelen {string} UserId dogrula.")
     public void response_den_gelen_user_id_dogrula(String string) {
@@ -70,3 +91,88 @@ public class US06_ApiStepDefs {
 
 
 }
+
+
+
+
+/*
+{ "birthDay": "1991-11-11",
+"birthPlace": "Deutschland",
+"gender": "MALE",
+"name": "Sevde",
+"password": "123456789Aa1.",
+"phoneNumber": "123-456-7890",
+"ssn": "123-32-7894",
+"surname": "Sevde",
+"username": "Sevde123"
+}
+
+
+{
+    "object": {
+        "userId": 468,
+        "username": "Sevde123",
+        "name": "Sevde",
+        "surname": "Sevde",
+        "birthDay": "1991-11-11",
+        "ssn": "123-32-7894",
+        "birthPlace": "Deutschland",
+        "phoneNumber": "123-456-7890",
+        "gender": "MALE"
+    },
+    "message": "Vice dean Saved",
+    "httpStatus": "CREATED"
+}
+ */
+
+/*
+package techproed.stepDefinition.api_step_defs;
+
+import io.cucumber.java.en.*;
+import io.restassured.response.Response;
+import techproed.pojos.dean_management.DeanPostPojo;
+import techproed.pojos.dean_management.ResponsePojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static techproed.base_urls.BaseUrl.spec;
+
+public class DeanManagement {
+    DeanPostPojo expectedData;
+    Response response;
+    ResponsePojo actualData;
+
+
+    @Given("Dean eklemek icin Post request hazirligi yapilir")
+    public void dean_eklemek_icin_post_request_hazirligi_yapilir() {
+        // Set the URL
+        spec.pathParams("first", "dean", "second", "save");
+    }
+    @Given("Gonderilecek dean bilgileri hazirlanir")
+    public void gonderilecek_dean_bilgileri_hazirlanir() {
+        // Set the expected data
+        expectedData = new DeanPostPojo("1975-05-05","Istanbul","MALE","Ali","12345678Aa*","505-555-5555","578-67-9518","Can","alicancancan");
+    }
+    @When("Dean eklemek icin Post request gonderilir")
+    public void dean_eklemek_icin_post_request_gonderilir() {
+        // Send the request and get the response
+        response = given(spec).body(expectedData).when().post("{first}/{second}");
+        actualData = response.as(ResponsePojo.class);
+
+    }
+    @Then("Dean bilgileri dogrulanir")
+    public void dean_bilgileri_dogrulanir() {
+        // Do assertion
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getBirthDay(), actualData.getObject().getBirthDay());
+        assertEquals(expectedData.getBirthPlace(), actualData.getObject().getBirthPlace());
+        assertEquals(expectedData.getGender(), actualData.getObject().getGender());
+        assertEquals(expectedData.getName(), actualData.getObject().getName());
+        assertEquals(expectedData.getPhoneNumber(), actualData.getObject().getPhoneNumber());
+        assertEquals(expectedData.getSsn(), actualData.getObject().getSsn());
+        assertEquals(expectedData.getSurname(), actualData.getObject().getSurname());
+        assertEquals(expectedData.getUsername(), actualData.getObject().getUsername());
+
+    }
+}
+ */
